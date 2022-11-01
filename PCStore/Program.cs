5a3fbf83eec22a678bc1a090f3ac4db03086b58d
GodLikeCSharpDeveloper <109728663+GodLikeCSharpDeveloper@users.noreply.github.com>
2022-11-01 11:39:45 +0200

    1st commit

--- PCStore/Program.cs
+++ PCStore/Program.cs
@@ -1,23 +1,37 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using PCStore.Data;
+using PCStore.Models;
+using PCStore.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+
+builder.Services.AddDbContext<StoreContext>(option =>
+                option.UseSqlServer(builder.Configuration.GetConnectionString("myconn")));
+builder.Services.AddScoped<StoreRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    
+        app.UseSwaggerUI();
+    }
+
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+
 
 // some changge
 
